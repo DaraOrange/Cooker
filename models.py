416d3f7ff113c8f6f5fdf7a3dffb86064d8d1c2c
Aida Ospanova <ospanovaida08@gmail.com>
2019-09-01 16:52:19 +0300

    Add cooker functionality and merge with OrangeBranch

--- models.py
+++ models.py
@@ -1,22 +1,25 @@
 class Order:
-	def __init__(self,orderId, planLeadTime, factLeadTime, factTime):
-		self.orderId = orderId
-		self.planLeadTime = planLeadTime
-		self.factTime = factTime
-		self.factLeadTime = factLeadTime
-
+	def __init__(self, orderName, factTime):
+                self.orderName = orderName
+                self.factTime = factTime
+        def __lt__(self, other):
+                return self.factTime < other.factTime
 
 class Employee:
-	def __init__(self, empId, userId, name)
+	def __init__(self, empId, userId, name) :
 		self.empId = empId
 		self.userId = userId
 		self.name = name
 
 class User:
-	def __init__(self, userId, role) 
+	def __init__(self, userId, role, status) :
 		self.userId = userId
 		self.role = role
-
+		self.status = status
 
 class Meal:
-	def __init__(self, mealId, planLeadTime, receipt, ingredients)
+	def __init__(self, mealId, planLeadTime, receipt, ingredients) :
+		self.mealId = mealId
+		self.planLeadTime = planLeadTime
+		self.receipt = receipt
+		self.ingredients = ingredients
