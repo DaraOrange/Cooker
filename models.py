8b5a04520c333b9e038cec9bbfa90fdd6dcdd58e
Aida Ospanova <ospanovaida08@gmail.com>
2019-09-01 14:16:45 +0300

    Change some models structure

--- models.py
+++ models.py
@@ -1,4 +1,22 @@
 class Order:
-	def __init__(self, planLeadTime, factLeadTime, factTime):	
+	def __init__(self,orderId, planLeadTime, factLeadTime, factTime):
+		self.orderId = orderId
 		self.planLeadTime = planLeadTime
 		self.factTime = factTime
+		self.factLeadTime = factLeadTime
+
+
+class Employee:
+	def __init__(self, empId, userId, name)
+		self.empId = empId
+		self.userId = userId
+		self.name = name
+
+class User:
+	def __init__(self, userId, role) 
+		self.userId = userId
+		self.role = role
+
+
+class Meal:
+	def __init__(self, mealId, planLeadTime, receipt, ingredients)
